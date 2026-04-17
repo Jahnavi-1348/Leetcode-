bd7a99d2f6ecb74dd774b6dc0ffa518c0f7c4ea2
Jahnavi-1348 <Jahnavi1348@gmail.com>
2026-04-17 16:48:06 -0500

    Time: 0 ms (100.00%) | Memory: 12.9 MB (85.02%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range( len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return i ,j
+        nummap={}
+        for i , num in enumerate(nums):
+            complement = target - num
+            if complement in nummap:
+                return [nummap[complement],i]
+            nummap[num] = i 
+            
+
+
+
